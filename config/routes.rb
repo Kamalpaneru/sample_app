035795c61feb4cc1f53cd05ccbba09c94e9937c8
Kamal <kamalpaneru.15@gmail.com>
2016-06-04 19:35:42 +0545

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
 
 
+match '/signup', :to => 'users#new'
 match '/about' , :to => 'pages#about'
 match '/contact' , :to => 'pages#contact'
 match '/help' , :to => 'pages#help'
+
 
 
 root :to => 'pages#home'
